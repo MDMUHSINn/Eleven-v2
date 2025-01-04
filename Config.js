ce7c2d3584c0c38616606d3d66e8f57bd9de99e2
MDMUHSINn <149122179+MDMUHSINn@users.noreply.github.com>
2025-01-04 16:32:22 +0300

    Update Config.js

--- Config.js
+++ Config.js
@@ -1,10 +1,10 @@
 const { ButtonStyle } = require("discord.js");
 module.exports = {
-    Token: "",
+    Token: "OTQ4OTQyMzI2Njk5NTI0MTc2.GE6mIE.vNju5ioPBo-5vTCYbyiJcP_NePOJPDeqx1_7rM",
     Prefix: ".",
     Client: {
-        ID: "",
-        Secret: "",
+        ID: "948942326699524176",
+        Secret: "8Z9V8uMH-mfZ0T6s5lkv2aXdQ1uH2mXH",
     },
     button: {
         grey: ButtonStyle.Secondary,
@@ -14,20 +14,20 @@ module.exports = {
         green: ButtonStyle.Success,
     },
     spotify: {
-        ID: "",
-        Secret: "",
+        ID: "e6f84fbec2b44a77bf35a20c5ffa54b8",
+        Secret: "498f461b962443cfaf9539c610e2ea81",
     },
     Api: {
         Topgg: "",
     },
-    MongoData: "",
-    EmbedColor: "",
-    Owners: [""],
+    MongoData: "mongodb+srv://arjunn:premiumop123@cluster0.dpw1dmi.mongodb.net/?retryWrites=true&w=majority",
+    EmbedColor: "#fffff",
+    Owners: ["921743618316836875"],
     Nodes: [
         {
-            name: "",
-            url: "",
-            auth: "",
+            name: "node",
+            url: "neko.pylex.xyz",
+            auth: "youshallnotpass",
             secure: false,
         },
         {
